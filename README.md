7be8471d5119fb5f5608256b1a014aaac7192a3d
wxd <wxd.sjtu@foxmail.com>
2018-06-29 23:54:25 +0800

    first version

---
+++ README.md
@@ -0,0 +1,8 @@
+# Description
+This is a simple smart pointer implementation, with basic functionality like `std::shared_ptr` in  C++ 11.
+
+
+Various introduction of `std::shared_ptr`, whether brief or detailed, are easily found in Internet or textbook.
+The principle and feature are not enough for beginners. Often, beginners, however, are unwilling to refer to the  source code, which is written with the goal of robustness but difficult to read. 
+
+Thus, this depository aims to provide a **clean implementation** for those beginners who has been fed the principle but want a deep understanding of `std::shared_ptr` from simple and specific implementation.
---
+++ smart_pointer.h
@@ -0,0 +1,145 @@
+#ifndef SmartPointer_h_
+#define SmartPointer_h_
+
+#include <iostream>
+#include <assert.h>
+
+/**********************************************ReferenceCounter******************************************************/
+class ReferenceCounter
+{
+protected:
+	unsigned int counter;
+public:
+	ReferenceCounter(): counter(0) {}
+
+	//! Destruktor, jesli counter != 0 znaczy, ze zostal zle policzony
+	~ReferenceCounter() { assert(counter == 0); } 
+	void Add() { counter++; }
+	unsigned int Remove() { return --counter; }
+	unsigned int getCounter() const { return counter; }
+};
+
+/***********************************************SmartPointer****************************************************/
+template <typename T>
+class SmartPointer
+{
+	template <typename U> friend class SmartPointer;
+public:
+	SmartPointer();
+	~SmartPointer() { Remove(); }
+
+	SmartPointer(T *other);	
+	template <typename U> SmartPointer(U *other);
+
+	SmartPointer(const SmartPointer<T> &other);
+	template <typename U> SmartPointer(const SmartPointer<U> &other);
+
+	SmartPointer& operator=(T* other);
+	template<typename U> SmartPointer& operator=(U *other);
+
+	T& operator*() const;
+	T* operator->() const;
+
+protected:
+	T *pointer;
+	ReferenceCounter *ref;
+
+	void Remove();
+};
+
+/***************************************************Inline********************************************************/
+
+//! Konstruktor domyslny
+template <typename T>
+SmartPointer<T>::SmartPointer()
+	:pointer(nullptr), ref(nullptr)
+{
+	ref = new ReferenceCounter;
+	ref->Add();
+}
+
+//! Konstruktor (T *other)
+template <typename T>
+SmartPointer<T>::SmartPointer(T *other) 
+	:pointer(other), ref(nullptr)
+{
+	ref = new ReferenceCounter;
+	ref->Add();
+}
+
+//! Konstruktor (U *other)
+template <typename T> template <typename U>
+SmartPointer<T>::SmartPointer(U * other)
+	:pointer(static_cast<T*>(other)), ref(nullptr)
+{
+	ref = new ReferenceCounter;
+	ref->Add();
+}
+
+//! Konstruktor kopiujacy przyjmujacy argument(const SmartPointer<T> &other)
+template <typename T>
+SmartPointer<T>::SmartPointer(const SmartPointer<T> &other)
+	:pointer(other.pointer), ref(other.ref)
+{
+	ref->Add();
+}
+
+//! Konstruktor kopiujacy przyjmujacy argument(const SmartPointer<U> &other)
+template <typename T> template <typename U>
+inline SmartPointer<T>::SmartPointer(const SmartPointer<U> &other)
+	:pointer(static_cast<T*>(other.pointer)), ref(other.ref)
+{
+	ref->Add();
+}
+
+//Operator= dla (T* other)
+template <typename T>
+SmartPointer<T>& SmartPointer<T>::operator=(T* other)
+{
+	if (pointer != other)
+	{
+		Remove();
+		pointer = other;
+		ref = new ReferenceCounter;
+		ref->Add();
+	}
+	return *this;
+}
+
+
+//Operator= dla (U* other)
+template<typename T> template<typename U>
+SmartPointer<T>& SmartPointer<T>::operator=(U *other)
+{
+	*this = static_cast<T*>(other); //this->operator=(static_cast<T*>(other));
+	return *this;
+}
+
+//! operator*
+template <typename T>
+T & SmartPointer<T>::operator*() const
+{
+	assert(pointer != nullptr);
+	return *pointer;
+}
+
+//! Operator->
+template <typename T>
+T * SmartPointer<T>::operator->() const
+{
+	assert(pointer != nullptr);
+	return pointer;
+}
+
+//! Zwalnia pointer oraz ref, jesli counter jest rowny zero
+template <typename T>
+inline void SmartPointer<T>::Remove()
+{
+	if (ref->Remove() == 0)
+	{
+		delete pointer;
+		delete ref;
+	}
+}
+
+#endif //SmartPointer_h_
